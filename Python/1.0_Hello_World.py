1fa1423de2f5ebba80c7e4ed853276c5a2891351
JDWhiskey1992 <j-daniells@hotmail.co.uk>
2015-09-18 17:13:04 +0100

    Added comments to '1.0_Hello_World.py' file.

--- Python/1.0_Hello_World.py
+++ Python/1.0_Hello_World.py
@@ -1,3 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3		
+
+"""After making the file executable in Ubuntu (chmod +x 1.0_Hello_World.py), the line 
+above allows the user to run the file via the command line by './1.0_Hello_World.py' 
+instead of 'python3 1.0_Hello_World.py'."""
 
 print("Hello World!")
+
+"""Unlike 'printf' in C and 'cout' in C++, the 'print' function in Python automatically ends the line at the end of the printed statement."""
